1c85660719182a26b2e1b5ad7065b9c15f1fef0d
Oskar <12718920+oskar2517@users.noreply.github.com>
2026-05-23 17:37:01 +0200

    some snake improvements

--- software/src/snake.cpp
+++ software/src/snake.cpp
@@ -17,12 +17,12 @@
 
 #define MAX_SNAKE_LENGTH (FIELD_ROWS * FIELD_COLS)
 
-typedef enum { UP, DOWN, LEFT, RIGHT } Direction;
+typedef enum { DIR_UP, DIR_DOWN, DIR_LEFT, DIR_RIGHT } Direction;
 
-typedef enum { GAME_OVER, PLAYING } GameState;
+typedef enum { GS_GAME_OVER, GS_PLAYING } GameState;
 
-static Direction dir = RIGHT;
-static GameState state = PLAYING;
+static Direction dir = DIR_RIGHT;
+static GameState state = GS_PLAYING;
 static uint8_t snake[MAX_SNAKE_LENGTH][2];
 static uint16_t fruit[2];
 static uint16_t len = 1;
@@ -57,7 +57,7 @@ static void draw_score() {
 }
 
 static void draw_game_over() {
-    if (state != GAME_OVER) return;
+    if (state != GS_GAME_OVER) return;
 
     video_out.setCursor(FIELD_X + FIELD_WIDTH / 2 - 55,
                         FIELD_Y + 3 * GRID_SIZE);
@@ -75,7 +75,7 @@ static void update_fruit() {
 }
 
 static void step_game() {
-    if (state == GAME_OVER) return;
+    if (state != GS_PLAYING) return;
 
     // Update snake tail
     for (uint16_t i = len - 1; i > 0; i--) {
@@ -85,16 +85,16 @@ static void step_game() {
 
     // Update snake head
     switch (dir) {
-        case UP:
+        case DIR_UP:
             snake[0][1] -= GRID_SIZE;
             break;
-        case DOWN:
+        case DIR_DOWN:
             snake[0][1] += GRID_SIZE;
             break;
-        case LEFT:
+        case DIR_LEFT:
             snake[0][0] -= GRID_SIZE;
             break;
-        case RIGHT:
+        case DIR_RIGHT:
             snake[0][0] += GRID_SIZE;
             break;
     }
@@ -115,51 +115,51 @@ static void step_game() {
     // Check lose condition: out of field bounds
     if (hx < FIELD_X || hx >= FIELD_X + FIELD_WIDTH || hy < FIELD_Y ||
         hy >= FIELD_Y + FIELD_HEIGHT) {
-        state = GAME_OVER;
+        state = GS_GAME_OVER;
         return;
     }
 
     // Check lose condition: self collision
     for (uint16_t i = 1; i < len; i++) {
         if (snake[i][0] == snake[0][0] && snake[i][1] == snake[0][1]) {
-            state = GAME_OVER;
+            state = GS_GAME_OVER;
             return;
         }
     }
 }
 
 void snake_handle_key(char read_key) {
-    if (state == GAME_OVER) {
+    if (state == GS_GAME_OVER) {
         reset_game();
     }
 
     switch (read_key) {
         case '8':
-            if (dir == DOWN) break;
-            dir = UP;
+            if (dir == DIR_DOWN) break;
+            dir = DIR_UP;
             break;
 
         case '2':
-            if (dir == UP) break;
-            dir = DOWN;
+            if (dir == DIR_UP) break;
+            dir = DIR_DOWN;
             break;
 
         case '4':
-            if (dir == RIGHT) break;
-            dir = LEFT;
+            if (dir == DIR_RIGHT) break;
+            dir = DIR_LEFT;
             break;
 
         case '6':
-            if (dir == LEFT) break;
-            dir = RIGHT;
+            if (dir == DIR_LEFT) break;
+            dir = DIR_RIGHT;
             break;
     }
 }
 
 static void reset_game() {
     len = 1;
-    dir = RIGHT;
-    state = PLAYING;
+    dir = DIR_RIGHT;
+    state = GS_PLAYING;
 
     // Init snake head to center of field
     snake[0][0] = FIELD_X + (FIELD_WIDTH / 2 - ((FIELD_WIDTH / 2) % GRID_SIZE));
